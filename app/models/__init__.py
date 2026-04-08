ed6999f26d6b69db66751d7fdeb1d4bc45c3af57
harshith2324 <90572358+harshith2324@users.noreply.github.com>
2026-04-08 17:28:14 -0400

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.mechanic import Mechanic
-from app.models.vehicle import Vehicle
-from app.models.spare_part import SparePart
-from app.models.service_request import ServiceRequest, JobUpdate
-from app.models.review import Review, Alert
